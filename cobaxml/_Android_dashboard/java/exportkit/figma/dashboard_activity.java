529691c931d24c0c3d7b7588147698fe536a3edb
Hafizh Luqman <luqmanulhakim020221@gmail.com>
2023-11-09 09:33:14 +0700

    dashbordxml

---
+++ cobaxml/_Android_dashboard/java/exportkit/figma/dashboard_activity.java
@@ -0,0 +1,86 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		dashboard
+	 *	@date 		Thursday 09th of November 2023 02:09:41 AM
+	 *	@title 		login
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class dashboard_activity extends Activity {
+
+	
+	private View _bg__dashboard_ek2;
+	private ImageView rectangle_38;
+	private ImageView rectangle_39;
+	private View _bg__group_64_ek1;
+	private View rectangle_4;
+	private View _bg__group_70_ek1;
+	private View _bg__group_66_ek1;
+	private View rectangle_4_ek1;
+	private TextView stock;
+	private View _bg__group_69_ek1;
+	private View _bg__group_65_ek1;
+	private View rectangle_4_ek2;
+	private TextView history;
+	private View _bg__group_71_ek1;
+	private View _bg__group_65_ek3;
+	private View rectangle_4_ek3;
+	private TextView kasir;
+	private ImageView rectangle_4_ek4;
+	private ImageView vector;
+	private ImageView vector_ek1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+
+		
+		_bg__dashboard_ek2 = (View) findViewById(R.id._bg__dashboard_ek2);
+		rectangle_38 = (ImageView) findViewById(R.id.rectangle_38);
+		rectangle_39 = (ImageView) findViewById(R.id.rectangle_39);
+		_bg__group_64_ek1 = (View) findViewById(R.id._bg__group_64_ek1);
+		rectangle_4 = (View) findViewById(R.id.rectangle_4);
+		_bg__group_70_ek1 = (View) findViewById(R.id._bg__group_70_ek1);
+		_bg__group_66_ek1 = (View) findViewById(R.id._bg__group_66_ek1);
+		rectangle_4_ek1 = (View) findViewById(R.id.rectangle_4_ek1);
+		stock = (TextView) findViewById(R.id.stock);
+		_bg__group_69_ek1 = (View) findViewById(R.id._bg__group_69_ek1);
+		_bg__group_65_ek1 = (View) findViewById(R.id._bg__group_65_ek1);
+		rectangle_4_ek2 = (View) findViewById(R.id.rectangle_4_ek2);
+		history = (TextView) findViewById(R.id.history);
+		_bg__group_71_ek1 = (View) findViewById(R.id._bg__group_71_ek1);
+		_bg__group_65_ek3 = (View) findViewById(R.id._bg__group_65_ek3);
+		rectangle_4_ek3 = (View) findViewById(R.id.rectangle_4_ek3);
+		kasir = (TextView) findViewById(R.id.kasir);
+		rectangle_4_ek4 = (ImageView) findViewById(R.id.rectangle_4_ek4);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
